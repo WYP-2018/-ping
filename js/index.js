1d3392ac554e2ccc73019a7d3957fb0a965f22d2
王亚鹏 <2642477288@qq.com>
2022-11-26 00:45:17 +0800

    '第二次修改'

--- js/index.js
+++ js/index.js
@@ -1,97 +1,100 @@
 // 监控模块
 (function () {
     // 监控模块
-    // 获取点击的tab choseTab
+    // 获取点击的tab  choseTab
     var choseTab = document.getElementsByClassName('choseTab');
-    // 获取下面对应的显示内容    showTab
-    var showTabs = document.getElementsByClassName("showTab");
-    for (var i = 0; i < choseTabs.length; i++) {
-        choseTabs[i].setAttribute('index', i);
+    // 获取下面对应的显示内容  showTab
 
-        choseTabs[i].onclick = function () {
-            var index = this.getAttribute('index');
-            console.log(index);
-            //排他法 切换选中的类名
-            for (var j = 0; j < choseTabs.length; j++) {
-                choseTabs[j].classList.remove('active');
-                choseTabs[index].classList.add('active');
+    var showTab = document.getElementsByClassName('showTab');
+
+    for (var i = 0; i < choseTab.length; i++) {
+        choseTab[i].setAttribute('index', i);
+
+        choseTab[i].onclick = function () {
+            var index_ = this.getAttribute('index');
+
+            for (var j = 0; j < choseTab.length; j++) {
+                choseTab[j].classList.remove('active');
+                choseTab[index_].classList.add('active');
             }
 
-            for (var k = 0; k < showTabs.length; k++) {
-                showTabs[k].style.display = 'none';
-                showTabs[index].style.display = 'block';
+            for (var k = 0; k < showTab.length; k++) {
+                showTab[k].style.display = 'none';
+                showTab[index_].style.display = 'block';
             }
-
         }
     }
-})()
-    // 自执行函数的好处：在其他的地方就可以使用相同的类名 而不会产生冲突可以控制变量的作用域
+
+})();
+
+
+// 自执行函数的好处：在其他的地方就可以使用相同的类名 而不会产生冲突可以控制变量的作用域
 
 
 
 
 
-    // 点位分布模块
-    (function () {
-        var myChart = echarts.init(document.querySelector('.pie'));
-        var option = {
-            color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
-            tooltip: {
-                // item 表述 鼠标到饼图的每一项内容的时候 触发提示信息
-                trigger: 'item',
-                // a:series 选项下面的name
-                // b:  series下面data中的name
-                // c:  series下面data中的value
-                // d:  当前value 占所以内容的比例
-                formatter: '{a} <br/>{b} : {c} ({d}%)'
-            },
-            series: [
-                {
-                    name: '老陈学员分布',
-                    // pie 饼图  
-                    type: 'pie',
-                    // 百分比 具体指都可以  百分比加一号
-                    // 1：内圆的半径  2 外圆的半径
-                    radius: ['10%', '65%'],
-                    // 设置水平方向  垂直方法  50% 居中
-                    center: ['50%', '50%'],
-                    roseType: 'radius',
-                    itemStyle: {
-                        borderRadius: 5
-                    },
-                    label: {
-                        fontSize: 10
-                    },
-                    labelLine: {
-                        length: 4,
-                        length2: 8
-                    },
-                    data: [
-                        { value: 20, name: '云南' },
-                        { value: 26, name: '北京' },
-                        { value: 24, name: '山东' },
-                        { value: 25, name: '河北' },
-                        { value: 20, name: '江苏' },
-                        { value: 25, name: '浙江' },
-                        { value: 30, name: '四川' },
-                        { value: 42, name: '河南' }
-                    ]
-                }
-            ]
-        };
-        myChart.setOption(option);
-        // 解决echarts设置option后，图标特别小的情况
-        // 页面加载的时候，调用echarts实例对象的reszie()方法
-        // 必须使用事件监听
-        window.addEventListener('load', function () {
-            myChart.resize();
-        });
-        // 当屏幕重置大小的时候  继续调用echarts的重置大小方法
-        // 自动适应当前屏幕
-        window.addEventListener('resize', function () {
-            myChart.resize();
-        })
-    })();
+// 点位分布模块
+(function () {
+    var myChart = echarts.init(document.querySelector('.pie'));
+    var option = {
+        color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
+        tooltip: {
+            // item 表述 鼠标到饼图的每一项内容的时候 触发提示信息
+            trigger: 'item',
+            // a:series 选项下面的name
+            // b:  series下面data中的name
+            // c:  series下面data中的value
+            // d:  当前value 占所以内容的比例
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        series: [
+            {
+                name: '老陈学员分布',
+                // pie 饼图  
+                type: 'pie',
+                // 百分比 具体指都可以  百分比加一号
+                // 1：内圆的半径  2 外圆的半径
+                radius: ['10%', '65%'],
+                // 设置水平方向  垂直方法  50% 居中
+                center: ['50%', '50%'],
+                roseType: 'radius',
+                itemStyle: {
+                    borderRadius: 5
+                },
+                label: {
+                    fontSize: 10
+                },
+                labelLine: {
+                    length: 4,
+                    length2: 8
+                },
+                data: [
+                    { value: 20, name: '云南' },
+                    { value: 26, name: '北京' },
+                    { value: 24, name: '山东' },
+                    { value: 25, name: '河北' },
+                    { value: 20, name: '江苏' },
+                    { value: 25, name: '浙江' },
+                    { value: 30, name: '四川' },
+                    { value: 42, name: '河南' }
+                ]
+            }
+        ]
+    };
+    myChart.setOption(option);
+    // 解决echarts设置option后，图标特别小的情况
+    // 页面加载的时候，调用echarts实例对象的reszie()方法
+    // 必须使用事件监听
+    window.addEventListener('load', function () {
+        myChart.resize();
+    });
+    // 当屏幕重置大小的时候  继续调用echarts的重置大小方法
+    // 自动适应当前屏幕
+    window.addEventListener('resize', function () {
+        myChart.resize();
+    })
+})();
 
 
 
